00dcccef3229d70f7b610e7f10ec19f822f55b4b
Appu Kumar <appukumar1204@gmail.com>
2026-08-21 12:28:33 +0530

    Update initial setup for AI itinerary service

--- services/ai-itinerary-service/src/main/java/com/planmytrip/ai_itinerary_service/AiItineraryServiceApplication.java
+++ services/ai-itinerary-service/src/main/java/com/planmytrip/ai_itinerary_service/AiItineraryServiceApplication.java
@@ -1,13 +1,13 @@
-package com.planmytrip.user_service;
+package com.planmytrip.ai_itinerary_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserServiceApplication {
+public class AiItineraryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		SpringApplication.run(AiItineraryServiceApplication.class, args);
 	}
 
 }
